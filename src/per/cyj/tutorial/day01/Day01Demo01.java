9e63a4361817263d19848d7188682a3b1bf6b234
cyjhunnyboy <cyjhunnyboy.com>
2020-09-02 18:02:26 +0800

    Java基础教程27天完整版

---
+++ src/per/cyj/tutorial/day01/Day01Demo01.java
@@ -0,0 +1,14 @@
+package per.cyj.tutorial.day01;
+
+/**
+ * 第一个Java程序
+ *
+ * @author chenyongjun
+ * @since 2020-02-05
+ */
+public class Day01Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/per/cyj/tutorial/day02/Day02Demo01.java
@@ -0,0 +1,20 @@
+package per.cyj.tutorial.day02;
+
+/**
+ * 关键字：被Java语言赋予特定含义的单词。
+ * <p>
+ * 特点：组成关键字单词的字母全部小写。
+ * <p>
+ * 注意：
+ * <br>A：goto和const是保留字
+ * <br>B：类似于Notepad++这样的高级记事本，针对关键字都有特殊的颜色标记
+ *
+ * @author chenyongjun
+ * @since 2020-02-05
+ */
+public class Day02Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
