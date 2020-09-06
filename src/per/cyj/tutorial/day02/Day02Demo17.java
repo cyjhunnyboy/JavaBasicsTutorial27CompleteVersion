9f49abcb5e7542ae5c0702ae56e309b836e73875
cyjhunnyboy <cyjhunnyboy@sina.com>
2020-09-06 18:38:54 +0800

    Java基础教程27天完整版

--- src/per/cyj/tutorial/day02/Day02Demo17.java
+++ src/per/cyj/tutorial/day02/Day02Demo17.java
@@ -9,12 +9,15 @@ package per.cyj.tutorial.day02;
 public class Day02Demo17 {
 
     public static void main(String[] args) {
-        // 字符串数据和其他数据做“+”，结果是字符串类型，这里的“+”不是加法运算，而是字符串连接符
-        System.out.println("hello" + 'a' + 1); // helloa1
-        System.out.println('a' + 1 + "hello"); // 98hello
-
-        System.out.println("5+5=" + 5 + 5); // 5+5=55
-        System.out.println(5 + 5 + "=5+5"); // 10=5+5
+        // 字符串数据和其他数据做“+”，结果是字符串类型
+        // 这里的“+”不是加法运算，而是字符串连接符
+        // 结果是：helloa1
+        System.out.println("hello" + 'a' + 1);
+        // 结果是：98hello
+        System.out.println('a' + 1 + "hello");
+        // 结果是：5+5=55
+        System.out.println("5+5=" + 5 + 5);
+        // 结果是：10=5+5
+        System.out.println(5 + 5 + "=5+5");
     }
 }
-
