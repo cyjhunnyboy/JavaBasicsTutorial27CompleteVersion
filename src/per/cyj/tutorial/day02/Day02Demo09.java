bf700dd93453bbb75595234c44519de2b2fb2bd4
cyjhunnyboy <cyjhunnyboy@sina.com>
2020-09-06 18:21:39 +0800

    Java基础教程27天完整版

--- src/per/cyj/tutorial/day02/Day02Demo09.java
+++ src/per/cyj/tutorial/day02/Day02Demo09.java
@@ -37,48 +37,48 @@ public class Day02Demo09 {
     public static void main(String[] args) {
         // 定义变量的格式：数据类型 变量名 = 初始化值;
         // 定义一个字节变量
-        byte byte1 = 10;
-        System.out.println(byte1);
+        byte b = 10;
+        System.out.println(b);
         System.out.println("---------------");
 
         // 定义一个短整型变量
-        short short1 = 100;
-        System.out.println(short1);
+        short s = 100;
+        System.out.println(s);
         System.out.println("---------------");
 
         // 定义一个整型变量
-        int int1 = 1000;
-        System.out.println(int1);
+        int i1 = 1000;
+        System.out.println(i1);
         // 超过了int的范围
-        // int int2 = 1000000000000;
-        // System.out.println(int2);
+        // int i2 = 1000000000000;
+        // System.out.println(i2);
         System.out.println("---------------");
 
         // 长整型后缀需要使用L或者l标记，建议L
-        long long1 = 1000000000000L;
-        System.out.println(long1);
+        long l1 = 1000000000000L;
+        System.out.println(l1);
         // long类型不管如何都记得加L
-        long long2 = 100L;
-        // long long2 = 100L;
-        System.out.println(long2);
+        // long l2 = 100;
+        long l2 = 100L;
+        System.out.println(l2);
         System.out.println("---------------");
 
         // 定义浮点数变量
-        float float1 = 12.234F;
-        System.out.println(float1);
-        double double1 = 12.345;
-        System.out.println(double1);
+        float f = 12.234F;
+        System.out.println(f);
+        double d = 12.345;
+        System.out.println(d);
         System.out.println("---------------");
 
         // 定义字符变量
-        char char1 = 'a';
-        System.out.println(char1);
-        char char2 = '中';
-        System.out.println(char2);
+        char c1 = 'a';
+        System.out.println(c1);
+        char c2 = '中';
+        System.out.println(c2);
         System.out.println("---------------");
 
         // 定义布尔变量
-        boolean bool1 = true;
-        System.out.println(bool1);
+        boolean bool = true;
+        System.out.println(bool);
     }
 }
